ed68873f012e5c542a6dd5524c1be053f412ac25
JesseAbram <jesseabramowitz@hotmail.com>
2022-04-22 13:03:32 -0400

    add network selection

--- blockchainServices/setup.ts
+++ blockchainServices/setup.ts
@@ -1,18 +1,19 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 const { Keyring } = require("@polkadot/keyring");
-const { cryptoWaitReady } = require('@polkadot/util-crypto');
-const { Ledger } = require('@polkadot/hw-ledger')
-import type { Signer, SignerResult } from '@polkadot/api/types';
-import type { Ledger as LedgerType } from '@polkadot/hw-ledger';
-import type { Registry, SignerPayloadJSON } from '@polkadot/types/types';
+const { cryptoWaitReady } = require("@polkadot/util-crypto");
+const { Ledger } = require("@polkadot/hw-ledger");
+import type { Signer, SignerResult } from "@polkadot/api/types";
+import type { Ledger as LedgerType } from "@polkadot/hw-ledger";
+import type { Registry, SignerPayloadJSON } from "@polkadot/types/types";
 
+const globalAny: any = global;
 
 const getApi = async () => {
   try {
-    const wsProvider = new WsProvider("wss://statemine-rpc.polkadot.io");
+    const wsProvider = new WsProvider(globalAny.network.endpoint);
     const api = new ApiPromise({
       types: {
-        "MultiAssets": "Vec<MultiAsset>"
+        MultiAssets: "Vec<MultiAsset>",
       },
       provider: wsProvider,
     });
@@ -36,13 +37,12 @@ const getKeypair = (mneumonic: string) => {
 };
 
 const getLedgerAddress = async () => {
-  const ledger = new Ledger('hid', 'statemine')
-  return await ledger.getAddress()
-  
-}
+  const ledger = new Ledger("hid", globalAny.network.name);
+  return await ledger.getAddress();
+};
 
-function isRawPayload (payload: any) {
-  return !!(payload).data;
+function isRawPayload(payload: any) {
+  return !!payload.data;
 }
 
 let id = 0;
@@ -53,32 +53,43 @@ class LedgerSigner implements Signer {
   readonly #getLedger: LedgerType;
   readonly #registry: Registry;
 
-  constructor (registry: Registry, getLedger: LedgerType, accountOffset: number, addressOffset: number) {
+  constructor(
+    registry: Registry,
+    getLedger: LedgerType,
+    accountOffset: number,
+    addressOffset: number
+  ) {
     this.#accountOffset = accountOffset;
     this.#addressOffset = addressOffset;
     this.#getLedger = getLedger;
     this.#registry = registry;
   }
 
-  public async signPayload (payload: SignerPayloadJSON): Promise<SignerResult> {
-    const raw = this.#registry.createType('ExtrinsicPayload', payload, { version: payload.version });
-    console.log({payload})
-    const { signature } = await this.#getLedger.sign(raw.toU8a(true), this.#accountOffset, this.#addressOffset);
+  public async signPayload(payload: SignerPayloadJSON): Promise<SignerResult> {
+    const raw = this.#registry.createType("ExtrinsicPayload", payload, {
+      version: payload.version,
+    });
+    console.log({ payload });
+    const { signature } = await this.#getLedger.sign(
+      raw.toU8a(true),
+      this.#accountOffset,
+      this.#addressOffset
+    );
 
     return { id: ++id, signature };
   }
-
-
 }
 
 const ledgerSignAndSend = async (call: any, api: any) => {
-  console.log("sending ledger transaction")
+  console.log("sending ledger transaction");
 
-  const ledger = new Ledger('hid', 'statemine')
-  const sender = await ledger.getAddress()
-  console.log({sender})
-  const ledgerSigner = new LedgerSigner(api.registry, ledger, 0, 0)
-  const signAsync = await call.signAsync(sender.address, {signer: ledgerSigner})
+  const ledger = new Ledger("hid", globalAny.network.name);
+  const sender = await ledger.getAddress();
+  console.log({ sender });
+  const ledgerSigner = new LedgerSigner(api.registry, ledger, 0, 0);
+  const signAsync = await call.signAsync(sender.address, {
+    signer: ledgerSigner,
+  });
   signAsync.send(({ status, events, dispatchError }: any) => {
     // status would still be set, but in the case of error we can shortcut
     // to just check it (so an error would indicate InBlock or Finalized)
@@ -102,39 +113,39 @@ const ledgerSignAndSend = async (call: any, api: any) => {
       }
     }
   });
-}
+};
 
 const signAndSend = (call: any, api: any, sender: any) => {
-  console.log("sending transaction")
+  console.log("sending transaction");
   call.signAndSend(sender, ({ status, events, dispatchError }: any) => {
-      // status would still be set, but in the case of error we can shortcut
-      // to just check it (so an error would indicate InBlock or Finalized)
-      if (dispatchError) {
-        if (dispatchError.isModule) {
-          // for module errors, we have the section indexed, lookup
-          const decoded = api.registry.findMetaError(dispatchError.asModule);
-          const { documentation, method, section } = decoded;
-  
-          console.log(`${section}.${method}: ${documentation.join(" ")}`);
-          process.exit();
-        } else {
-          // Other, CannotLookup, BadOrigin, no extra info
-          console.log(dispatchError.toString());
-          process.exit();
-        }
+    // status would still be set, but in the case of error we can shortcut
+    // to just check it (so an error would indicate InBlock or Finalized)
+    if (dispatchError) {
+      if (dispatchError.isModule) {
+        // for module errors, we have the section indexed, lookup
+        const decoded = api.registry.findMetaError(dispatchError.asModule);
+        const { documentation, method, section } = decoded;
+
+        console.log(`${section}.${method}: ${documentation.join(" ")}`);
+        process.exit();
       } else {
-        if (status.isFinalized) {
-          console.log("transaction successful");
-          process.exit();
-        }
+        // Other, CannotLookup, BadOrigin, no extra info
+        console.log(dispatchError.toString());
+        process.exit();
       }
-    });
-  }
+    } else {
+      if (status.isFinalized) {
+        console.log("transaction successful");
+        process.exit();
+      }
+    }
+  });
+};
 
 module.exports = {
   getApi,
   getKeypair,
   signAndSend,
   ledgerSignAndSend,
-  getLedgerAddress
+  getLedgerAddress,
 };
